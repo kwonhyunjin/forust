22b7009a28379ddecddc4ab501e8d33973ca6f71
Beryl <mniberyl@gmail.com>
2020-07-04 19:13:15 +0900

    Add next-seo.config.js

---
+++ next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+  title: 'Forust',
+  description: "커뮤니티에 참여하고 문제를 해결하세요.",
+  openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    site_name: 'Forust',
+    description: "커뮤니티에 참여하고 문제를 해결하세요.",
+  },
+  twitter: {
+    cardType: 'summary_large_image',
+  },
+}
