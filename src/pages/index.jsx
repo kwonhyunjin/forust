f55833bbd6ad77828a909b3c694089fbb0d0a76e
Beryl <mniberyl@gmail.com>
2020-07-29 01:18:46 +0900

    Add router setting to index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,8 @@
+import ForumList from '@/pages/forum/list/index';
 import React from 'react';
 
 export default function Home() {
   return (
-    <div className="app" />
+    <ForumList />
   );
 }
