dcd78beb1de3971177dd45f4e2978a51fa761816
Beryl <mniberyl@gmail.com>
2020-07-29 15:23:33 +0900

    Fix AppNav component

--- src/components/app-nav/app-nav.jsx
+++ src/components/app-nav/app-nav.jsx
@@ -1,37 +1,39 @@
 import React from 'react';
+import Participation from '../../svgs/icon-chat.svg';
+import Solved from '../../svgs/icon-check-circle.svg';
+import Unsolved from '../../svgs/icon-close-circle.svg';
+import Help from '../../svgs/icon-help-circle.svg';
 
 export default function AppNav() {
   return (
     <>
-      <nav className="app-nav" id="nav-heading">
-        <h2 className="visually-hidden">Your Commutiny</h2>
-        <ul className="nav-menu" role="listbox" aria-label="nav-heading" aria-activedescendant="help" tabIndex="0">
-          <li className="nav-menu__items" role="option" aria-selected="true" id="help">
-            <a href="/forum-list" className="nav-menu__link">
-              <img src="/images/navigation-help.svg" alt="All Question" className="nav-menu__img" />
-              All Questions
-            </a>
-          </li>
-          <li className="nav-menu__items" role="option" aria-selected="false">
-            <a href="/forum-list" className="nav-menu__link">
-              <img src="/images/navigation-participation.svg" alt="My Participation" className="nav-menu__img" />
-              My Participation
-            </a>
-          </li>
-          <li className="nav-menu__items" role="option" aria-selected="false">
-            <a href="/forum-list" className="nav-menu__link">
-              <img src="/images/navigation-unsolved.svg" alt="Unsolved" className="nav-menu__img" />
-              Unsolved
-            </a>
-          </li>
-          <li className="nav-menu__items" role="option" aria-selected="false">
-            <a href="/forum-list" className="nav-menu__link">
-              <img src="/images/navigation-solved.svg" alt="Solved" className="nav-menu__img" />
-              Solved
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <ul className="app-nav" role="listbox" aria-labelledby="app-nav">
+        <h2 className="visually-hidden" id="app-nav">AppNav</h2>
+        <li className="app-nav__item">
+          <a href="/forum-list" className="app-nav__link is-active" aria-current="page">
+            <Help className="app-nav__img" />
+            All Questions
+          </a>
+        </li>
+        <li className="app-nav__item">
+          <a href="/forum-list" className="app-nav__link">
+            <Participation className="app-nav__img" />
+            My Participation
+          </a>
+        </li>
+        <li className="app-nav__item">
+          <a href="/forum-list" className="app-nav__link">
+            <Unsolved className="app-nav__img" />
+            Unsolved
+          </a>
+        </li>
+        <li className="app-nav__item">
+          <a href="/forum-list" className="app-nav__link">
+            <Solved className="app-nav__img" />
+            Solved
+          </a>
+        </li>
+      </ul>
     </>
   );
 }
