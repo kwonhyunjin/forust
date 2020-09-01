7b5ed30404001d7d4dec7ee895e3e87dba8cb657
Beryl <mniberyl@gmail.com>
2020-09-01 18:48:16 +0900

    Add `Anchor` type to Button component

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,16 +2,21 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function Button({
+const Button = React.forwardRef(({
   children, className, type = 'button', ...rest
-}) {
+}, ref) => {
+  const isAnchor = type === 'anchor';
+  const Element = isAnchor ? 'a' : 'button';
+
   return (
-    <button {...rest} className={classNames('button', className)} type={type}>{children}</button>
+    <Element {...rest} className={classNames('button', className)} type={isAnchor ? undefined : type} ref={ref}>{children}</Element>
   );
-}
+});
 
 Button.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
   type: PropTypes.string,
 };
+
+export default React.memo(Button);
