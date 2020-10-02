a2977bec93681bd2cd2ac05e305e4b5409476889
Beryl <mniberyl@gmail.com>
2020-10-02 15:36:17 +0900

    Fix compile error

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -2,6 +2,11 @@ import Document, { Head, Main, NextScript } from 'next/document';
 import React from 'react';
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
       <html lang="ko">
