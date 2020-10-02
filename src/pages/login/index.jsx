5e18460a051e34c236cc976b6f1a6fa9690459a9
Beryl <mniberyl@gmail.com>
2020-10-02 15:52:24 +0900

    Implement login, signup pages

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,16 +1,36 @@
 import Button from '@/components/button/button';
-import Checkbox from '@/components/checkbox/checkbox';
 import FormField from '@/components/form-field/form-field';
-import Label from '@/components/label/label';
 import TextField from '@/components/text-field/text-field';
+import firebase from '@/firebase/index';
 import AuthLayout from '@/layouts/auth-layout/auth-layout';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const pageTitle = 'Login | Forust';
 
-export default function Login() {
+const Login = () => {
+  const router = useRouter();
+  const {
+    register, setError, errors, handleSubmit,
+  } = useForm({
+    mode: 'onChange',
+  });
+
+  const handleFormSubmit = async (data) => {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(data.email, data.password);
+      router.push('/');
+    } catch (err) {
+      setError('password', {
+        type: err.code,
+        message: err.message,
+      });
+    }
+  };
+
   return (
     <>
       <NextSeo
@@ -21,27 +41,44 @@ export default function Login() {
       />
       <h1 className="blind">Forust</h1>
       <AuthLayout>
-        <div className="auth-form">
+        <form className="auth-form" onSubmit={handleSubmit(handleFormSubmit)}>
           <h2 className="auth-form-heading">Login to Forust</h2>
           <div className="grid-row form-row">
             <div className="grid-col">
-              <FormField label="Username or Email">
-                <TextField type="text" id="username" />
+              <FormField label="Email" error={errors.email && errors.email.message}>
+                <TextField
+                  type="text"
+                  name="email"
+                  ref={
+                    register({
+                      required: 'this is required',
+                      pattern: {
+                        value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                        message: 'Invalid email address',
+                      },
+                    })
+                  }
+                />
               </FormField>
             </div>
           </div>
           <div className="grid-row form-row">
             <div className="grid-col">
-              <FormField label="Password">
-                <TextField type="password" id="password" />
+              <FormField label="Password" error={errors.password && errors.password.message}>
+                <TextField
+                  type="password"
+                  name="password"
+                  ref={
+                    register({
+                      required: 'this is required',
+                      minLength: {
+                        value: 6,
+                        message: 'Min length is 6',
+                      },
+                    })
+                  }
+                />
               </FormField>
-            </div>
-          </div>
-          <div className="grid-row form-row">
-            <div className="grid-col">
-              <Label input={<Checkbox />}>
-                Keep me logged in.
-              </Label>
             </div>
           </div>
           <div className="grid-row auth-form-submit-row">
@@ -58,8 +95,10 @@ export default function Login() {
               </Link>
             </div>
           </div>
-        </div>
+        </form>
       </AuthLayout>
     </>
   );
-}
+};
+
+export default Login;
--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -1,14 +1,43 @@
 import Button from '@/components/button/button';
 import FormField from '@/components/form-field/form-field';
 import TextField from '@/components/text-field/text-field';
+import firebase from '@/firebase/index';
 import AuthLayout from '@/layouts/auth-layout/auth-layout';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const pageTitle = 'Create Account | Forust';
 
-export default function Signup() {
+const Signup = () => {
+  const router = useRouter();
+  const {
+    register, setError, clearErrors, errors, handleSubmit,
+  } = useForm({
+    mode: 'onChange',
+  });
+
+  const handleFormSubmit = async (data) => {
+    try {
+      await firebase.auth().createUserWithEmailAndPassword(data.email, data.password);
+      setTimeout(async () => {
+        const user = firebase.auth().currentUser;
+        await user.updateProfile({
+          displayName: data.displayName,
+        });
+        router.push('/');
+      }, 200);
+    } catch (err) {
+      // @todo Alert 컴포넌트 개발
+      setError('email', {
+        type: err.code,
+        message: err.message,
+      });
+    }
+  };
+
   return (
     <>
       <NextSeo
@@ -19,37 +48,64 @@ export default function Signup() {
       />
       <h1 className="blind">Forust</h1>
       <AuthLayout>
-        <form className="auth-form">
+        <form className="auth-form" onSubmit={handleSubmit(handleFormSubmit)}>
           <h2 className="auth-form-heading">Create your Forust Account</h2>
           <div className="grid-row form-row">
             <div className="grid-col">
-              <FormField label="Name">
-                <TextField type="text" id="name" />
-              </FormField>
-            </div>
-            <div className="grid-col">
-              <FormField label="Username">
-                <TextField type="text" id="username" />
+              <FormField label="Name" error={errors.displayName && errors.displayName.message}>
+                <TextField
+                  type="text"
+                  name="displayName"
+                  ref={
+                    register({
+                      required: 'this is a required',
+                      maxLength: {
+                        value: 10,
+                        message: 'Max length is 10',
+                      },
+                    })
+                  }
+                />
               </FormField>
             </div>
           </div>
           <div className="grid-row form-row">
             <div className="grid-col">
-              <FormField label="Email">
-                <TextField type="text" id="email" />
+              <FormField label="Email" error={errors.email && errors.email.message}>
+                <TextField
+                  type="text"
+                  name="email"
+                  ref={register({
+                    required: 'this is required',
+                    pattern: {
+                      value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                      message: 'Invalid email address',
+                    },
+                  })}
+                />
               </FormField>
             </div>
           </div>
           <div className="grid-row form-row">
             <div className="grid-col">
-              <FormField label="Password">
-                <TextField type="password" id="password" />
+              <FormField label="Password" error={errors.password && errors.password.message}>
+                <TextField
+                  type="password"
+                  name="password"
+                  ref={register({
+                    required: 'this is required',
+                    minLength: {
+                      value: 6,
+                      message: 'Min length is 6',
+                    },
+                  })}
+                />
               </FormField>
             </div>
           </div>
           <div className="grid-row auth-form-submit-row">
             <div className="grid-col">
-              <Button className="auth-submit" type="submit">Create Account</Button>
+              <Button className="auth-submit" type="submit" onClick={() => clearErrors()}>Create Account</Button>
             </div>
           </div>
           <div className="grid-row auth-form-desc-row">
@@ -63,4 +119,6 @@ export default function Signup() {
       </AuthLayout>
     </>
   );
-}
+};
+
+export default Signup;
