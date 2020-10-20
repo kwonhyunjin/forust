b88397312c6b0c5da7e78120445e7b6d2b0562ee
Beryl <mniberyl@gmail.com>
2020-10-20 14:52:06 +0900

    Memoize icon type of Alert component

--- src/components/alert/alert.jsx
+++ src/components/alert/alert.jsx
@@ -1,37 +1,33 @@
 import Icon from '@/components/icon/icon';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useMemo } from 'react';
+
+const getIconType = (type) => {
+  if (type === 'success') {
+    return 'check-circle-outline';
+  }
+  if (type === 'error') {
+    return 'alert-octagon-outline';
+  }
+  if (type === 'warning') {
+    return 'alert-triangle-outline';
+  }
+  return 'info-circle-outline';
+};
 
 export default function Alert({
   children, className, type = 'info', ...rest
 }) {
+  const iconType = useMemo(() => getIconType(type), [type]);
   return (
     <div {...rest} className={classNames('alert', `alert-${type}`, className)}>
-      {type === 'info' && (
-        <>
-          <Icon type="info-circle-outline" className="alert__icon alert__icon--info" aria-hidden="true" />
-          <div className="alert__message">{children}</div>
-        </>
-      )}
-      {type === 'success' && (
-        <>
-          <Icon type="check-circle-outline" className="alert__icon alert__icon--success" aria-hidden="true" />
-          <div className="alert__message">{children}</div>
-        </>
-      )}
-      {type === 'error' && (
-        <>
-          <Icon type="alert-octagon-outline" className="alert__icon alert__icon--error" aria-hidden="true" />
-          <div className="alert__message">{children}</div>
-        </>
-      )}
-      {type === 'warning' && (
-        <>
-          <Icon type="alert-triangle-outline" className="alert__icon alert__icon--warning" aria-hidden="true" />
-          <div className="alert__message">{children}</div>
-        </>
-      )}
+      <Icon
+        type={iconType}
+        className={classNames('alert__icon', `alert__icon--${type}`)}
+        aria-hidden="true"
+      />
+      <div className="alert__message">{children}</div>
     </div>
   );
 }
