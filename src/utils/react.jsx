cefa81996f7816a91a806e26a18514980e00db8d
Beryl <mniberyl@gmail.com>
2020-12-11 21:33:18 +0900

    Add `isRenderable` function

--- src/utils/react.jsx
+++ src/utils/react.jsx
@@ -4,3 +4,9 @@ export const TIMESTAMP_PROP_TYPES = PropTypes.shape({
   seconds: PropTypes.number,
   nanoseconds: PropTypes.number,
 });
+
+export function isRenderable(node) {
+  if (node === 0) { return true; }
+  if (typeof node === 'boolean') { return false; }
+  return !!node;
+}
